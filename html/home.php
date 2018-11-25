8c567403c0232b91877c1135fbd5a93984c4b89e
RaffyB <raffybautista07@gmail.com>
2018-11-24 21:19:19 -0500

    start of home page

---
+++ html/home.php
@@ -0,0 +1,77 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <!-- Navbar-->
+    <div class="topnav">
+    <ul>
+        <li><a class="active" href="home.php">Home</a></li>
+        <li><a href="products.php">Shoes</a></li>
+        <li><a href="">About</a></li>
+        <li class = "login" id="loginBtn">Account</li>
+        
+    </ul>
+    </div>
+    <!--Modal for login-->
+    <div class="modal-login">
+        <!-- Modal content -->
+        <div class="modal-login-content">
+        <span class="loginClose">&times;</span>
+        <p>Some text in the Modal..</p>
+        </div>
+    </div>
+        
+    
+
+    <!-- Content-->
+    <div class="container">
+        <div class="row">
+            <div class="column-1">
+                ...
+            </div>
+            <div class="column-2">
+                ...
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="column-2">
+                ...
+            </div>
+            <div class="column-1">
+                ...
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom bar-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ html/modal.js
@@ -0,0 +1,66 @@
+var modal_login = document.getElementById('modal-login');
+var loginbtn = document.getElementById("loginBtn");
+var loginspan = document.getElementsByClassName("loginClose")[0];
+
+// When the user clicks on the button, open the modal 
+loginbtn.onclick = function() {
+    modal_login.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+loginspan.onclick = function() {
+    modal_login.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal_login) {
+        modal_login.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------
+var x=0;
+// Get the modal
+var modal = document.getElementsByClassName('modal');
+
+
+// Get the button that opens the modal
+var img = document.getElementsByClassName("img")[1];
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+
+// When the user clicks on the button, open the modal 
+img.onclick = function() {
+    modal[1].style.display = "block";
+   
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal[1].style.display = "none";
+    
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal[1]) {
+        modal[1].style.display = "none";
+        
+    }
+}
+
+
+

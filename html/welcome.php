6efe2d99872f8b5290decf347379cba6e8fdb2a3
MandoCuesta <45244821+MandoCuesta@users.noreply.github.com>
2018-11-27 14:26:51 -0500

    Update welcome.php

--- html/welcome.php
+++ html/welcome.php
@@ -20,6 +20,19 @@
       <p class="content">
       This is <?php echo $_SESSION["user"] ?>'s account page.
       </p>
+      
+      <img src"" alt = "User Wallpaper Picture" width = "auto" height = "540px"> /* User should be able to upload profile image and wallpaper image to his acount database or choose one from defaults*/
+      <img src"" alt = "User Profile Picture Here" width = "200px" height= "200px"> 
+      <div>
+               <p>Your Recent Activity</p> /* From Cookies we should be able to look at history and show what user has looked at.*/
+         <ul>
+            <li>You looked @ img 1</li> 
+            <li>You looked @ img 2</li>
+            <li>You looked @ img 3</li>
+         </ul>
+      </div>
+     
+
 
       <button class="logout_btn" name ="logout">Logout</button>
    </form>

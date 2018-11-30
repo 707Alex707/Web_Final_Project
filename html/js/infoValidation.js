6f59f08f0a90f56905b30b5d1db452abf79e59ed
Anson Tu <anson9137@gmail.com>
2018-11-30 04:58:05 -0500

    Add files via upload
    Added functions to validate the user's billing and payment information.

--- html/js/infoValidation.js
+++ html/js/infoValidation.js
@@ -10,13 +10,13 @@ function validBillingInfo() {
     var email = document.getElementById("email").value;
     var address = document.getElementById("adr").value;
     var city = document.getElementById("city").value;
-    var province = document.getElementById("province").value;
+    var province = document.getElementById("state").value;
     var postalCode = document.getElementById("zip").value;
-    var nameCheck = /^[a-z ,.'-]+$/i;
-    var addressCheck = /^[A-Za-z0-9'\.\-\s\,]/;
+    var nameCheck = /^[A-Z]{1}[a-z]{1,} [A-Z]{1}[a-z]{1,}$/;
+    var addressCheck = /^[0-9]{0,5} [A-Z]{1}[a-z]{1,} [A-Z]{1}[a-z]{1,}$/;
     var cityCheck = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
     var provinceCheck = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
-    var postalCheck = /^[0-9A-Za-z]{6}$/;
+    var postalCheck = /^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/;
     if (name == "" || email == "" || address == "" || city == "" || province == "" || postalCode == "") {
         event.preventDefault();
         alert("One or more fields need to be filled");
@@ -35,17 +35,43 @@ function validBillingInfo() {
     } else if (!postalCheck.exec(postalCode)) {
         event.preventDefault();
         alert("Invalid postal code");
-    } else {
-        return true;
     }
 }
 
 function validCreditInfo() {
-    
+    var cName = document.getElementById("cname").value;
+    var cardNum = document.getElementById("ccname").value;
+    var expMonth = document.getElementById("expmonth").value;
+    var expYear = document.getElementById("expyear").value;
+    var ccv = document.getElementById("ccv").value;
+    var cNameCheck = /^[A-Z]{1}[a-z]{1,} [A-Z]{1}[a-z]{1,}$/;
+    var cardNumCheck = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/;
+    var expMonthCheck = /^January$|February$|March$|April$|May$|June$|July$|August$|September$|October$|November$|December$/
+    var expYearCheck = /^201[8-9]{1}$|20[2-9]{1}[0-9]{1}$/;
+    var ccvCheck = /^[0-9]{3}$/;
+    debugger;
+    if (cName == "" || cardNum == "" || expMonth == "" || expYear == "" || ccv == "") {
+        event.preventDefault();
+        alert("One or more fields need to be filled");
+    } else if (!cNameCheck.exec(cName)) {
+        event.preventDefault();
+        alert("Invalid credit card owner name");
+    } else if (!cardNumCheck.exec(cardNum)) {
+        event.preventDefault();
+        alert("Invalid credit card number");
+    } else if (!expMonthCheck.exec(expMonth)) {
+        event.preventDefault();
+        alert("Invalid card expiry month");
+    } else if (!expYearCheck.exec(expYear)) {
+        event.preventDefault();
+        alert("Invalid card expiry year");
+    } else if (!ccvCheck.exec(ccv)) {
+        event.preventDefault();
+        alert("Invalid card ccv");
+    }
 }
 
 function validInfo() {
-    if (validBillingInfo() && validCreditInfo() == true) {
-
-    }
+    validBillingInfo();
+    validCreditInfo();
 }
